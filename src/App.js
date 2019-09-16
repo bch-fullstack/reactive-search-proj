1c407bf4a2cd384a057d7649ad6c2b6d65f4ff76
Hoang Pham <tienhoang.pham@bc.fi>
2019-09-16 10:27:45 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+import { ReactiveBase, ReactiveList, ResultCard } from '@appbaseio/reactivesearch';
 import './App.css';
 
+const { ResultCardsWrapper } = ReactiveList;
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ReactiveBase
+			app="football_player_lookup"
+			credentials="8Gh0pKVof:c437efc3-00e4-409e-ada8-1fd2d94dd559"
+		>
+			<ReactiveList
+				componentId="SearchResult"
+				renderItem={res => <div>{res.Name}</div>}
+			>
+				{({ data, error, loading }) => (
+					<ResultCardsWrapper>
+						{
+							data.map(item => (
+								<ResultCard key={item.ID}>
+									<ResultCard.Image src={item.Photo} />
+									<ResultCard.Title
+										dangerouslySetInnerHTML={{
+											__html: item.Name
+										}}
+									/>
+									<ResultCard.Description>
+										<div>
+											<div>{item.Nationality}</div>
+											<div>
+												({item.Overall} avg)
+                                		</div>
+										</div>
+										<span>
+											{item.Club}
+										</span>
+									</ResultCard.Description>
+								</ResultCard>
+							))
+						}
+					</ResultCardsWrapper>
+				)}
+			</ReactiveList>
+
+		</ReactiveBase>
+	);
 }
 
 export default App;
