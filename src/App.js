1476427ac014c7ff4c766ea83b5ea52a6a67a8e9
Hoang Pham <tienhoang.pham@bc.fi>
2019-09-16 10:36:27 +0300

    add searching component and update result accordingly

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ReactiveBase, ReactiveList, ResultCard } from '@appbaseio/reactivesearch';
+import { ReactiveBase, ReactiveList, ResultCard, DataSearch } from '@appbaseio/reactivesearch';
 import './App.css';
 
 const { ResultCardsWrapper } = ReactiveList;
@@ -10,9 +10,23 @@ function App() {
 			app="football_player_lookup"
 			credentials="8Gh0pKVof:c437efc3-00e4-409e-ada8-1fd2d94dd559"
 		>
+			<DataSearch
+				componentId="SearchSensor"
+				dataField="Name"
+				title="Search by Name"
+				placeholder="Search for football player by name"
+				autosuggest={true}
+				highlight={true}
+				queryFormat="or"
+				fuzziness={0}
+				debounce={100}
+			/>
 			<ReactiveList
 				componentId="SearchResult"
 				renderItem={res => <div>{res.Name}</div>}
+				react={{
+					and: ['SearchSensor'],
+				}}
 			>
 				{({ data, error, loading }) => (
 					<ResultCardsWrapper>
